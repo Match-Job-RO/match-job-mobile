6a67c9c8e1a4d7ba4e5cfdbea40e522a5ace8414
Daniel_Dyn <102476758+DanieL-S22@users.noreply.github.com>
2023-10-04 18:57:05 -0400

    feat: Singup front-end done

--- src/Navgator/Navgator.js
+++ src/Navgator/Navgator.js
@@ -1,15 +1,16 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "../screens/Login";
 import Home from "../screens/Home";
+import Signup from "../screens/Signup";
 
 const Stack = createStackNavigator();
 
 export default function Navigator() {
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}} >
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Home" component={Home} />
-      {/* <Stack.Screen name="Signup" component={Signup} /> */}
+      <Stack.Screen name="Signup" component={Signup} />
     </Stack.Navigator>
   );
 }
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,19 +1,21 @@
 import { useState } from "react";
-import { View, Text, TextInput, Button, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { login } from "../services/loginService";
+import { signup } from "../services/signupService";
 
-export default function Login() {
+export default function Signup() {
   const navgation = useNavigation();
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  async function handleLogin() {
+  async function handleSignup() {
     const userData = {
+      name,
       email,
       password,
     };
-    const data = await login(userData);
+    const data = await signup(userData);
     navgation.navigate("Home", { data });
   }
 
@@ -21,7 +23,7 @@ export default function Login() {
     <View className="mx-auto">
       <View className="mx-auto mt-16 p-8 text-center">
         <Text className="text-black text-4xl font-normal font-['Comfortaa']">
-          Login
+          Signup
         </Text>
       </View>
 
@@ -33,12 +35,22 @@ export default function Login() {
 
       <View>
         <View className="p-4">
+          <Text>Nome</Text>
+          <TextInput
+            placeholder="Nome"
+            value={name}
+            onChangeText={setName}
+            className="border-2 border-black mt-2 p-1 px-2 rounded-md"
+          />
+        </View>
+
+        <View className="p-4">
           <Text>Email</Text>
           <TextInput
             placeholder="Email"
             value={email}
             onChangeText={setEmail}
-            className="border-2 border-black mt-2 p-1 px-2 rounded-md"
+            className="border-2 border-black p-1 px-2 rounded-md"
           />
         </View>
 
@@ -55,15 +67,7 @@ export default function Login() {
       </View>
 
       <View className="p-4">
-        <TouchableOpacity onPress={() => navgation.navigate("Signup")}>
-          <Text style={{ color: "blue", textDecorationLine: "underline" }}>
-            Cadastre-se
-          </Text>
-        </TouchableOpacity>
-      </View>
-
-      <View className="p-4">
-        <Button title="Entrar" onPress={handleLogin} />
+        <Button title="Entrar" onPress={handleSignup} />
       </View>
     </View>
   );
