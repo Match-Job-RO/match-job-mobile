9c3caa290e3ed440bced078880cc2b984d028455
magnojunior07 <magnojunior319@gmail.com>
2023-12-04 22:57:42 -0400

    refac: add headerShown

--- src/Navgator/Navigator.tsx
+++ src/Navgator/Navigator.tsx
@@ -12,63 +12,63 @@ const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function AuthStack() {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Signup" component={Signup} />
-      <Stack.Screen name="Post" component={Post} />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator screenOptions={{ headerShown: false }}>
+			<Stack.Screen name="Home" component={Home} />
+			<Stack.Screen name="Login" component={Login} />
+			<Stack.Screen name="Signup" component={Signup} />
+			<Stack.Screen name="Post" component={Post} />
+		</Stack.Navigator>
+	);
 }
 function MainStack() {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Map" component={Map} />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator screenOptions={{ headerShown: false }}>
+			<Stack.Screen name="Home" component={Home} />
+			<Stack.Screen name="Map" component={Map} />
+		</Stack.Navigator>
+	);
 }
 function TabNavigator() {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+	return (
+		<Tab.Navigator
+			screenOptions={({ route }) => ({
+				headerShown: false,
+				tabBarIcon: ({ focused, color, size }) => {
+					let iconName;
 
-          if (route.name === "Home") {
-            iconName = focused ? "home" : "home-outline";
-          } else if (route.name === "Map") {
-            iconName = focused ? "map" : "map-outline";
-          }
+					if (route.name === "Home") {
+						iconName = focused ? "home" : "home-outline";
+					} else if (route.name === "Map") {
+						iconName = focused ? "map" : "map-outline";
+					}
 
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: "blue",
-        inactiveTintColor: "gray",
-        style: "bg-white",
-      }}
-    >
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Map" component={Map} />
-    </Tab.Navigator>
-  );
+					return <Ionicons name={iconName} size={size} color={color} />;
+				},
+			})}
+			tabBarOptions={{
+				activeTintColor: "blue",
+				inactiveTintColor: "gray",
+				style: "bg-white",
+			}}>
+			<Tab.Screen name="Home" component={Home} />
+			<Tab.Screen name="Map" component={Map} />
+		</Tab.Navigator>
+	);
 }
 export default function RootNavigator() {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-        name="Auth"
-        component={AuthStack}
-        options={{ animationEnabled: false }}
-      />
-      <Stack.Screen
-        name="Main"
-        component={TabNavigator}
-        options={{ animationEnabled: false }}
-      />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator screenOptions={{ headerShown: false }}>
+			<Stack.Screen
+				name="Auth"
+				component={AuthStack}
+				options={{ animationEnabled: false }}
+			/>
+			<Stack.Screen
+				name="Main"
+				component={TabNavigator}
+				options={{ animationEnabled: false }}
+			/>
+		</Stack.Navigator>
+	);
 }
