0fd1687198edd59ae17b973fa3c91e55c50952fd
Victor-Prudente <victoreduprudente@gmail.com>
2023-12-05 00:55:57 -0400

    feature: profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,33 +1,64 @@
-import React, { useState } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  Button,
-  ScrollView,
-  SafeAreaView,
-} from "react-native";
-import { IUserData } from "../intefarces/user.interface";
+import React, { useState } from 'react';
+import { View, TextInput, Button, Text, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
 
 export default function Profile() {
-  const [name, setName] = useState<IUserData>("");
-  const [image, setImage] = useState<string>("");
-  const [genre, setGenre] = useState<string>("");
+  const [name] = useState('Usuário');
+  const [bio, setBio] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [tags, setTags] = useState('');
+  const [genre, setGenre] = useState('Masculino');
 
-  const handleOrder = () => {};
+  const handleUpdate = () => {
+    
+  };
 
   return (
     <ScrollView>
       <SafeAreaView className="p-16 h-full">
         <View className="flex justify-center items-center py-16">
-          <View className="mb-8">
+          <View className="p-4">
             <Text className="font-light text-4xl text-purple p-4">
-              OLÁ {name}
+              Olá {name}
             </Text>
           </View>
-          <Button title="Meus Pedidos" onPress={handleOrder} />
+            <View className="gap-8">
+              <TextInput
+                className="border border-gray-500 p-2 mb-4"
+                placeholder="Bio"
+                value={bio}
+                onChangeText={setBio}
+              />
+              <TextInput
+                className="border border-gray-500 p-2 mb-4"
+                placeholder="Telefone"
+                value={phone}
+                onChangeText={setPhone}
+              />
+              <TextInput
+                className="border border-gray-500 p-2 mb-4"
+                placeholder="Endereço"
+                value={address}
+                onChangeText={setAddress}
+              />
+              <TextInput
+                className="border border-gray-500 p-2 mb-4"
+                placeholder="Tags"
+                value={tags}
+                onChangeText={setTags}
+              />
+              <View className="flex-row justify-between mb-4">
+                <TouchableOpacity onPress={() => setGenre('Masculino')}>
+                  <Text className="${genre === 'Masculino' && 'font-bold'}">Masculino</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => setGenre('Feminino')}>
+                  <Text className="${genre === 'Feminino' && 'font-bold'}">Feminino</Text>
+                </TouchableOpacity>
+              </View>
+          </View>
+          <Button title="Atualizar" onPress={handleUpdate} />
         </View>
       </SafeAreaView>
     </ScrollView>
   );
-}
+};
